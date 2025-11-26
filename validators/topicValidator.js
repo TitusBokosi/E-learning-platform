56d0423434ff5b40a74b141318fd0da902812696
Michael Makawa <programmer01mj@gmail.com>
2025-11-26 20:37:38 +0200

    topic-validator first implementation of topic validators. (#12)

--- validators/topicValidator.js
+++ validators/topicValidator.js
@@ -1,17 +1,46 @@
-const {body, param} = require('express-validator');
+const z= require("zod")
 
-    const createTopicValidator = [
-    ]
+    const createTopicValidator = z.object({
+        content:z
+            .string()
+            .min(100)
+            .max(10000)
+            .trim(),
+        courseId:z
+            .string()
+            .uuid("provide a valid courseId")
+            .trim(),
+    })
 
-    const getTopicByIdValidator = [
+    const getTopicByIdValidator = z.object({
+        topicId:z
+            .string()
+            .uuid("provide a valide topicId")
+            .trim(),    
+    })
 
-    ]
+    const updateTopicValidator = z.object({
+        topicId:z
+            .string()
+            .uuid("provide a valid topicId")
+            .trim(),
+        courseId:z
+            .string()
+            .uuid("povide a valid topicId")
+            .trim(),
+        topicName:z
+            .string()
+            .min(5)
+            .max(100)
+            .trim()
+    });
 
-    const updateTopicValidator = [
-    ]
-
-    const deleteTopicValidator = [
-    ]
+    const deleteTopicValidator = z.object({
+        topicId:z
+            .string()
+            .uuid("provide a valid topicId")
+            .trim(),
+    })
     
 module.exports = {
     createTopicValidator,
