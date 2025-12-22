6fcb9723025d56de89c3a703fb01247e5aa6c290
Michael Makawa <programmer01mj@gmail.com>
2025-12-22 06:10:32 +0200

    queries. updated the admin, lessons, and topics queries. -2

--- queries/admin.js
+++ queries/admin.js
@@ -1,12 +1,20 @@
 const prisma = require("../config/db");
 
 const changeUserRole = (id, data)=>{
+    return prisma.User.update({
+        where:{id},
+        data,
+    })
 
 }
 
 const deleteUser = id =>{
+    return prisma.User.delete({
+        where:{id},
+    })
 
 }
+
 
 module.exports = {
     changeUserRole,
--- queries/lessons.js
+++ queries/lessons.js
@@ -1,22 +1,38 @@
 const prisma = require("../config/db");
 
 const createLesson = (data)=>{
+    return prisma.Lesson.create({
+        data,
+    })
 
 }
 
 const getAllLessons = (isPublished = true )=>{
+    return prisma.Lesson.findMany({
+        where:{isPublished}
+    })
 
 }
 
 const getLessonById = (id)=>{
+    return prisma.Lesson.findUnique({
+        where:{id},
+    })
 
 }
 
 const updateLesson = (id, data) => {
+    return prisma.Lesson.update({
+        where:{id},
+        data,
+    })
 
 }
 
 const deleteLesson = (id) =>{
+    return prisma.lesson.delete({
+        where:{id},
+    })
 
 }
 
