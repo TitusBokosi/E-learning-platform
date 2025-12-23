5929a6ebdd8750e22d30ed1cde6c651db29908b6
Titus <titusbokosi@gmail.com>
2025-12-23 09:40:30 +0200

    feat(error-handler): added error handler middleware (#3)

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,3 +1,31 @@
-const errorHandler = (err, req, res, next) => {};
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = err.statusCode || 500;
+    const status = err.status || "error";
+
+    if (process.env.NODE_ENV === "development") {
+        res.status(statusCode).json({
+            status: status,
+            message: err.message,
+            stack: err.stack,
+            error: err
+        });
+    } else if (process.env.NODE_ENV === "production") {
+        if (err.isOperational){
+            res.status(statusCode).json({
+                status,
+                message: err.message
+            });
+        }
+        else{
+            res.status(500).json({
+                status,
+                message: "Something went wrong!"
+            });
+        }
+    }
+};
+
+    
 
 module.exports = { errorHandler };
