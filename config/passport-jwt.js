95b2077efc4dd4d81e337c215b32571264a7082e
Titus <titusbokosi@gmail.com>
2025-12-23 21:06:23 +0200

    feat(passport-jwt): configured passport jwt (#5)

---
+++ config/passport-jwt.js
@@ -0,0 +1,52 @@
+
+const { getUserById } = require("../queries/users");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtratcJWT = require("passport-jwt").ExtractJwt;
+
+module.exports = passport => {
+
+  const accessTokenOptions = {
+    jwtFromRequest: ExtratcJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey:process.env.ACCESS_TOKEN_SECRET,
+  };
+
+  passport.use(
+    "jwt-access",
+    new JWTStrategy(accessTokenOptions, async(payload, done)=>{
+      try{
+        const user = await getUserById(payload.id);
+
+        if(user){
+          return done(null, user)
+        }
+
+        return done(null, false);
+      }
+      catch(err){
+        return done(err, false);
+      }
+    })
+  )
+  const refreshTokenOptions = {
+    jwtFromRequest: req => req.cookies?.refreshToken,
+    secretOrKey:process.env.REFRESH_TOKEN_SECRET,
+  };
+
+  passport.use(
+    "jwt-refresh",
+    new JWTStrategy(refreshTokenOptions, async(payload, done) =>{
+      try{
+        const user = await getUserById(payload.id);
+
+      if(user){
+        return done(null, user);
+      }
+
+      return done (null, false);
+      }
+      catch(err){
+        return done (err, false);
+      }
+    })
+  )
+}
