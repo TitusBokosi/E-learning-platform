dbf2376c7a54330f54623010fb5078be5be453e9
Titus <titusbokosi@gmail.com>
2025-12-25 12:05:24 +0200

    feat(auth) added auth middleware (#6)

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,51 @@
-const protect = async (req, res, next) => {};
+const passport = require("passport");
 
-const adminOnly = (req, res, next) => {};
+exports.authenticateLocal = (req, res, next) => {
+    passport.authenticate("local", {session: false}, (err, user, info) =>{
+    if (err)
+        return next(err);
 
-module.exports = { protect, adminOnly };
+    if(!user){
+        return res.status(401).json({
+            status: "fail",
+            message: info.message || "Authentication failed",
+        })
+    }
+    req.user =user;
+    next();
+}) (req, res, next);
+}
+
+exports.authenticateAccessToken = (req, res, next) =>{
+    passport.authenticate("jwt-access", {session:false}, (err, user, info) =>{
+        if(err)
+            return next (err);
+
+        if(!user){
+            return res.status(401).json({
+                status:"fail",
+                message: info.message || "Unauthorized access"
+            })
+        }
+
+        req.user = user;
+        next();
+    })(req, res, next);
+}
+
+exports.authenticateRefreshToken = (req, res, next) => {
+    passport.authenticate("jwt-refresh", {session: false}, (err, user, info)=>{
+        if(err)
+            return next(err);
+
+        if(!user){
+            return res.status(401).json({
+                status:"fail",
+                message: info.message || "Unauthorized access",
+            })
+        }
+
+        req.user =user;
+        next();
+    })(req, res , next);
+}
