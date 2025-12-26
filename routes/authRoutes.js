8bba7dfe43910a40f410cdb62ee45faab69e31e5
Titus <titusbokosi@gmail.com>
2025-12-26 10:28:35 +0200

    feat(auth) implimeneted auth routes (#8)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
 const authRoute = express.Router();
-
+const {registerUser, loginUser, refreshTokenController, updatePasswordController, logOutUserController} = require("../controllers/authControllers");
+const {authenticateLocal, authenticateAccessToken, authenticateRefreshToken} = require ("../middlewares/authMiddleware");
+authRoute.post("/register", registerUser);
+authRoute.post("/login", authenticateLocal, loginUser);
+authRoute.post("/refresh" ,authenticateRefreshToken, refreshTokenController);
+authRoute.post("/logout", logOutUserController);
+authRoute.patch("/password", authenticateAccessToken, updatePasswordController);
 
 module.exports = authRoute;
