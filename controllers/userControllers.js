610ee6ca0f67d0544e17a43031b860251ec4ca6e
Titus <titusbokosi@gmail.com>
2025-12-28 15:57:46 +0200

    feat(controllers) implimented controlles (#29)

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,19 +3,77 @@ const { createUser, getUserById, getAllUsers, updateUser, deleteUser } = require
 
 
 
-const getAllUsersController = async (req, res) => {};
+const getAllUsersController = async (req, res, next) => {
+  try {
+    const users = await getAllUsers();
+    res.status(200).json({
+      status: "success",
+      data: users
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getUserByIdController = async (req, res) => {};
+const getUserByIdController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
 
-const deleteUserController = async (req, res) => {};
+    if (!user) {
+      return next(new AppError("User not found", 404));
+    }
 
-const changeUserRoleController = async (req, res) => {};
+    return res.status(200).json({
+      status: "success",
+      data: user
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteUserController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUser(id);
+
+    if (!deletedUser) {
+      return next(new AppError("User not found", 404));
+    }
+
+    return res.status(204).json({
+      status: "success",
+      data: null
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUserController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    
+    const updatedUser = await updateUser(id, data);
+
+    return res.status(200).json({
+      status: "success",
+      data: updatedUser
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// const changeUserRoleController = async (req, res) => {};
 
 module.exports = {
-  registerUser,
-  loginUser,
+  
   getAllUsersController,
   getUserByIdController,
   deleteUserController,
-  changeUserRoleController
+  updateUserController
+
 };
