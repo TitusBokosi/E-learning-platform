2efcf58127e7ade1bf0c0b6746fb13e5e20a11a3
Michael Makawa <programmer01mj@gmail.com>
2025-12-30 04:16:05 +0200

    course request validation- first implementation of course validators (#11)

--- validators/courseValidator.js
+++ validators/courseValidator.js
@@ -1,17 +1,65 @@
-const {body, param} = require('express-validator');
+const {z }=require("zod");
 
-    const createCourseValidator = [
+    const createCourseValidator =z.object({
+        title:z
+            .string()
+            .min(5)
+            .max(225)
+            .trim(),
+        description:z
+            .string()
+            .min(20)
+            .max(225)
+            .trim(),
+        category:z
+            .string.min(2)
+            .max(200)
+            .trim(),
+        level: z
+            .enum(["beginner", "intermediate", "advanced", "all levels"]),
+        toltalLessons:z
+            .number()
+            .min(1)
+            .max(500)
+            .default(1),
+        instructorId:z
+            .string()
+            .uuid()
+            .min(1),
+        courseStatus:z
+            .boolean()
+            .default(false),
+    }) ;
 
-    ]
+    const getCourseByIdValidator = z.object({
+        courseId:z
+            .string()
+            .uuid("must be a valid courseId")
+    });
 
-    const getCourseByIdValidator = [
-    ]
+    const updateCourseValidator = z.object({
+        courseId:z
+            .string()
+            .uuid("should be a valid courseId"),
+            newdata: z.object({
+                title,
+                descripion,
+                category,
+                level,
+                toltalLessons,
+                instructorId,
+                courseStatus
 
-    const updateCourseValidator = [
-    ]
+            })
+    });
 
-    const deleteCourseValidator = [
-    ]
+    const deleteCourseValidator = z.object({
+        courseId:z
+            .string()
+            .uuid('invalid courseId'),
+        courseTitle:z
+            .string()
+    });
 
 module.exports = {
     createCourseValidator,
