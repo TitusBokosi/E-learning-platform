21106594a81c3db834903a8972078ed390acb816
Michael Makawa <programmer01mj@gmail.com>
2026-01-02 04:18:30 +0200

    feat(coursesRoutes): first implementation of coursesRoutes (#16)

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { createCourseController, getAllCoursesController, getCourseByIdController, updateCourseController, deleteCourseController} =require("./controllers/courseControllers");
+const {createCourseValidator, getCourseByIdValidator, updateCourseValidator, deleteCourseValidator,} =require ("../validators/courseValidator");
 
 const courseRoute = express.Router();
 
+courseRoute.GET("/", getAllCoursesController);
+courseRoute.POST("/", validate(createCourseValidator), createCourseController);
+courseRoute.GET("/:courseid", validate(getCourseByIdValidator), getCourseByIdController);
+courseRoute.PUT("/:courseid", validate(updateCourseValidator), updateCourseController);
+courseRoute.DELETE("/:courseid", validate(deleteCourseValidator), deleteCourseController);
 
 module.exports = courseRoute;
