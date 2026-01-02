0f7cb08c61e58f6ab75c215d244d66f0206100cf
Titus <titusbokosi@gmail.com>
2026-01-02 15:18:55 +0200

    fixes

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const topicRoute = require("./topicRoutes");
 
 const courseRoute = express.Router();
+
+courseRoute.use("/:courseId/topics", topicRoute);
 
 
 module.exports = courseRoute;
