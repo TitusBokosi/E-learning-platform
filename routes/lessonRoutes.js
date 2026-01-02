89ccc757ede4e8c6ff0b60026f0b79735144171f
Michael Makawa <programmer01mj@gmail.com>
2026-01-02 05:14:41 +0200

    feat(lesson-Routes): first implementation (#18)

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const {validate} = require("../middlewares/validateRequest");
+const {createLessonValidator, getLessonByIdValidator, updateLessonValidator, deleteLessonValidator,} = require("../validators/lessonValidator");
+const {createLessonController, getAllLessonsController, getLessonByIdController, updateLessonController, deleteLessonController} = require("../controllers/lessonController");
 
- const lessonRoute = express.Router()
+const lessonRoute = express.Router()
 
+lessonRoute.get("/lessons", getAllLessonsController);
+lessonRoute.post("/createlesson", validate(createLessonValidator), createLessonController);
+lessonRoute.get("/lessonid", validate(getLessonByIdValidator), getLessonByIdController);
+lessonRoute.patch("/updatelesson", validate(updateLessonValidator), updateLessonController);
+lessonRoute.delete("/deletelesson", validate(deleteLessonValidator), deleteLessonController);
 
  module.exports = lessonRoute;
